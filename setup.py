4bbc372e2359e92fe2b516689220779ac129311c
Chris Mower <chris.mower@ed.ac.uk>
2022-05-01 12:38:48 +0100

    Package'ify pyinvk

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyinvk',
+    version='1.0',
+    description='Python Inverse Kinematics.',
+    url='https://github.com/cmower/pyinvk',
+    author='Christopher E. Mower',
+    author_email='cmower@ed.ac.uk',
+    license='BSD 2-Clause License',
+    packages=['pyinvk'],
+)
