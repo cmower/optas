5059ff2fd1620db71c46a088d90ce76eba9df19b
Chris E. Mower <chris.mower@ed.ac.uk>
2022-05-02 13:30:42 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyinvk',
-    version='1.0',
+    version='1.0.0',
     description='Python Inverse Kinematics.',
     url='https://github.com/cmower/pyinvk',
     author='Christopher E. Mower',
