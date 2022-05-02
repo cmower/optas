a7c62cae88349f630eb1cf72b8c74fc1130ef9e5
Chris E. Mower <chris.mower@ed.ac.uk>
2022-05-02 13:40:36 +0100

    Add long description, and bug tracker

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyinvk',
     version='1.0.0',
     description='Python Inverse Kinematics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/cmower/pyinvk',
+    project_urls={
+        "Bug Tracker": "https://github.com/cmower/pyinvk/issues",
+    },
     author='Christopher E. Mower',
     author_email='cmower@ed.ac.uk',
     license='BSD 2-Clause License',
