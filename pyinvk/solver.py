28ad8d8b751042121b70771bfae6c9fa39e82ece
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-06-22 18:49:03 +0100

    Add casadi QP solver and optional OSQP interface

--- pyinvk/solver.py
+++ pyinvk/solver.py
@@ -1,7 +1,17 @@
+import numpy as np
 import casadi as cs
 from abc import ABC, abstractmethod
 from scipy.optimize import minimize, NonlinearConstraint, LinearConstraint
 from scipy.sparse import csr_matrix
+from .optimization import UnconstrainedQP,\
+    LinearConstrainedQP, \
+    NonlinearConstrainedQP, \
+    UnconstrainedOptimization, \
+    LinearConstrainedOptimization, \
+    NonlinearConstrainedOptimization
+
+################################################################
+# Solver base class
 
 class Solver(ABC):
 
@@ -10,24 +20,24 @@ class Solver(ABC):
     def __init__(self, optimization):
         self.optimization = optimization
         self.x0 = cs.DM.zeros(optimization.nx)
-        self.p = self.optimization.parameters.dict2vec({})
+        self.p = cs.DM.zeros(optimization.np)
 
     @abstractmethod
     def setup(self, *args, **kwargs):
-        """Initialize solver, called in __init__"""
+        """Setup solver, must return self"""
         pass
 
     def reset_initial_seed(self, x0):
         if isinstance(x0, dict):
             self.x0 = self.optimization.decision_variables.dict2vec(x0)
         else:
-            self.x0 = cs.DM(x0)
+            self.x0 = cs.vec(cs.DM(x0))
 
     def reset_parameters(self, p):
         if isinstance(p, dict):
             self.p = self.optimization.parameters.dict2vec(p)
         else:
-            self.p = cs.DM(p)
+            self.p = cs.vec(cs.DM(p))
 
     @abstractmethod
     def solve(self):
@@ -39,33 +49,148 @@ class Solver(ABC):
         """Return stats from solver"""
         pass
 
-class CasadiNLPSolver(Solver):
+################################################################
+# CasADi solvers (https://web.casadi.org/)
 
-    """Casadi solver"""
+class CasADiSolver(Solver):
+
+    """
+
+    This is a base class for CasADi solvers. The following
+    attributes are expected to be set in the setup method:
+    - is_constrained
+    - _solver
+    - _lbg/_ubg (when is_constrained is True)
+
+    """
+
+    def solve(self):
+        solver_input = {
+            'x0': self.x0,
+            'p': self.p,
+        }
+        if self.is_constrained:
+            solver_input['lbg'] = self._lbg
+            solver_input['ubg'] = self._ubg
+        solution = self._solver(**solver_input)
+        return self.optimization.decision_variables.vec2dict(solution['x'])
+
+    def stats(self):
+        return self._solver.stats()
+
+class CasADiQPSolver(CasADiSolver):
+
+    """CasADi QP solver"""
 
     def setup(self, solver_name, solver_options={}):
-        """Setup casadi nlp solver"""
 
-        x = cs.SX.sym('x', self.optimization.nx)
-        p = cs.SX.sym('p', self.optimization.np)
+        err_msg = "CasadiQPSolver cannot solve this problem\n\nSee https://web.casadi.org/docs/#quadratic-programming"
+        opt_type = type(self.optimization)
+        assert opt_type in {UnconstrainedQP, LinearConstrainedQP}, err_msg
+
+        x = self.optimization.decision_variables.vec()
+        p = self.optimization.parameters.vec()
 
         problem = {
             'x': x,
             'p': p,
             'f': self.optimization.f(x, p),
-            'g': self.optimization.u(x, p),
         }
-        self.solver = cs.nlpsol('solver', solver_name, problem, solver_options)
+        self._lbg = None
+        self._ubg = None
+        self.is_constrained = opt_type == LinearConstrainedQP
+        if self.is_constrained:
+            problem['g'] = self.optimization.k(x, p)
+            self._lbg = self.optimization.lbk
+            self._ubg = self.optimization.ubk
 
-        self.lbg = self.optimization.lbu
-        self.ubg = self.optimization.ubu
+        self._solver = cs.qpsol('solver', solver_name, problem, solver_options)
+
+        return self
+
+class CasADiNLPSolver(CasADiSolver):
+
+    """Casadi NLP solver"""
+
+    def setup(self, solver_name, solver_options={}):
+        """Setup casadi nlp solver"""
+
+        x = self.optimization.decision_variables.vec()
+        p = self.optimization.parameters.vec()
+
+        problem = {
+            'x': x,
+            'p': p,
+            'f': self.optimization.f(x, p),
+        }
+
+        opt_type = type(self.optimization)
+        self.is_constrained = opt_type not in {UnconstrainedQP, UnconstrainedOptimization}
+        self._lbg = None
+        self._ubg = None
+        if self.is_constrained:
+            problem['g'] = self.optimization.u(x, p)
+            self._lbg = self.optimization.lbu
+            self._ubg = self.optimization.ubu
+
+        self._solver = cs.nlpsol('solver', solver_name, problem, solver_options)
+
+        return self
+
+################################################################
+# OSQP solver (https://osqp.org/)
+
+class OSQPSolver(Solver):
+
+    def setup(self, use_warm_start=True, settings={}):
+
+        opt_type = type(self.optimization)
+        assert opt_type in {UnconstrainedQP, LinearConstrainedQP}, "OSQP cannot solve this type of problem, see https://osqp.org/docs/solver/index.html"
+
+        osqp_installed = True
+        try:
+            import osqp
+        except ImportError:
+            osqp_installed = False
+
+        assert osqp_installed, "could not import osqp, is it installed? Use $ pip install osqp"
+
+        self.osqp = osqp
+        self.use_warm_start = use_warm_start
+        self.is_constrained = opt_type == LinearConstrainedQP
+
+        return self
 
     def solve(self):
-        sol = self.solver(x0=self.x0, p=self.p, lbg=self.lbg, ubg=self.ubg)
-        return self.optimization.decision_variables.vec2dict(sol['x'])
+
+        # Setup problem
+        setup_input = {
+            'P': csr_matrix(self.optimization.P(self.p).toarray()),
+            'q': self.optimization.q(self.p).toarray().flatten(),
+        }
+        if self.is_constrained:
+            setup_input['l'] = self.optimization.lbr(self.p).toarray().flatten()
+            setup_input['u'] = np.inf*np.ones(self.optimization.nr)
+            setup_input['A'] = csr_matrix(self.optimization.M(self.p).toarray())
+        self.m = self.osqp.OSQP()
+        self.m.setup(**setup_input)
+
+        # Warm start optimization
+        if self.use_warm_start:
+            self.m.warm_start(x=self.x0.toarray().flatten())
+
+        # Solve problem
+        self._stats = self.m.solve()
+        solution = self._stats.x
+
+        self.first_solve = False
+        return self.optimization.decision_variables.vec2dict(solution)
 
     def stats(self):
-        return self.solver.stats()
+        return self._stats
+
+################################################################
+# Scipy Minimize solvers (https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html)
 
 class ScipyMinimizeSolver(Solver):
 
