b1c069660b5d1cf15757862eb60ff2485b3667af
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-06-25 01:29:28 +0100

    Add docstrings

--- pyinvk/builder.py
+++ pyinvk/builder.py
@@ -1,4 +1,7 @@
 import casadi as cs
+import numpy as np
+from typing import Dict, List, Optional, Union
+from .robot_model import RobotModel
 from .sx_container import SXContainer
 from .optimization import UnconstrainedQP,\
     LinearConstrainedQP, \
@@ -9,9 +12,40 @@ from .optimization import UnconstrainedQP,\
 
 class OptimizationBuilder:
 
-    """Class that builds an optimization problem"""
+    """The OptimizationBuilder class is used to build an optimization problem."""
 
-    def __init__(self, robots, T=1, qderivs=[0]):
+    def __init__(
+            self,
+            robots: Dict[str, RobotModel],
+            T: Optional[int]=1,
+            qderivs: Optional[List[int]]=[0]
+    ):
+        """Constructor for the OptimizationBuilder class.
+
+        Parameters
+        ----------
+
+        robots : dict[str:pyinvk.robot_model.RobotModel]
+            A dictionary containing the robot models in the
+            scene. Each model should be indexed by a unique name for
+            the robot. If you are interfacing with the ROS-PyBullet
+            Interface then the robot name should be set to the same
+            name as is given in the interface configuration file.
+
+        T : int (default is 1)
+            Number of time-steps in the trajectory to be treated as
+            decicision variables. The value of T should be strictly
+            positive and satisfy the inequality T > max(qderivs).
+
+        qderivs : list[int] (default is [0])
+            A list containing the derivative orders that should be
+            included in the decision variables. For example, [0, 1]
+            will include the joint configuration trajectory (i.e. a
+            vector q with T time-steps), and the joint velocities
+            (i.e. a vector qd with T-1 time-steps). All values in
+            qderivs should be positive or zero.
+
+        """
 
         # Input check
         assert min(qderivs) >= 0, "All values in qderivs should be positive or zero"
@@ -40,29 +74,177 @@ class OptimizationBuilder:
         self.eq_constraints = SXContainer()
 
     @staticmethod
-    def statename(robot_name, qderiv):
+    def statename(robot_name: str, qderiv: int) -> str:
+        """Returns the state name for a given time derivative of q.
+
+        The state name is used to index the decision variables.
+
+        Parameters
+        ----------
+
+        robot_name : str
+            Name of the robot, as given in the robots parameter in the
+            class constructor.
+
+        qderiv : int
+            The derivative order.
+
+        Returns
+        -------
+
+        state_name : str
+            A string in the format "{robot_name}/[D]q" where "[D]" is
+            qderiv-number of "d"'s, e.g. if robot_name="robot" and
+            qderiv=2, then the state name will be "robot/qdd".
+
+        """
         return robot_name + '/' + 'd'*qderiv + 'q'
 
-    def get_state(self, robot_name, t, qderiv=0):
+    def get_state(
+            self,
+            robot_name: str,
+            t: int,
+            qderiv: Optional[int]=0) -> cs.casadi.SX:
+        """Return the configuration state for a given time deriviative of q.
+
+        Parameters
+        ----------
+
+        robot_name : str
+            Name of the robot, as given in the robots parameter in the
+            class constructor.
+
+        t : int
+            Time step in the trajectory.
+
+        qderiv : int
+            The derivative order.
+
+        Returns
+        -------
+
+        state : casadi.casadi.SX
+            The state for a given robot, at a given time derivative,
+            for a given time-step.
+
+        """
         assert qderiv in self.qderivs, f"{qderiv=}, qderiv must be in {self.qderivs}"
         states = self.decision_variables[self.statename(robot_name, qderiv)]
         return states[:, t]
 
-    def add_decision_variables(self, name, m=1, n=1):
+    def add_decision_variables(
+            self,
+            name: str,
+            m: Optional[int]=1,
+            n: Optional[int]=1) -> cs.casadi.SX:
+        """Add a decision variable to the optimization problem.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the decision variable.
+
+        m : int (default is 1)
+            Number of rows in the decision variable array.
+
+        n : int (default is 1)
+            Number of columns in the decision variable array.
+
+        Returns
+        -------
+
+        decision_variable : casadi.casadi.SX
+            The decision variable SX array.
+        """
         x = cs.SX.sym(name, m, n)
         self.decision_variables[name] = x
         return x
 
-    def add_cost_term(self, name, cost_term):
+    def add_cost_term(self, name: str, cost_term: cs.casadi.SX) -> None:
+        """Add a cost term to the optimization problem.
+
+        When the optimization problem is built, the cost function is
+        given by a sum of cost terms.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the cost term.
+
+        cost_term : casadi.casadi.SX
+            The cost term as a SX variable. Note, this must be scalar,
+            i.e. with shape (1, 1).
+
+        """
         assert cost_term.shape == (1, 1), "cost terms must be scalars"
         self.cost_terms[name] = cost_term
 
-    def add_parameter(self, name, m=1, n=1):
+    def add_parameter(self, name: str, m: Optional[int]=1, n : Optional[int]=1) -> cs.casadi.SX:
+        """Add a parameter to the optimization problem.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the parameter. Note, this name is used when
+            referencing the parameter for the reset_parameters method
+            in the Solver class.
+
+        m : int (default is 1)
+            Number of rows in the parameter array.
+
+        n : int (default is 1)
+            Number of columns in the parameter array.
+
+        Returns
+        -------
+
+        param : casadi.casadi.SX
+            The SX parameter array.
+
+        """
         p = cs.SX.sym(name, m, n)
         self.parameters[name] = p
         return p
 
-    def add_lin_constraint(self, name, lbc, c, ubc):
+    def add_lin_constraint(
+            self,
+            name: str,
+            lbc: Union[cs.casadi.SX, cs.casadi.DM],
+            c: cs.casadi.SX,
+            ubc: Union[cs.casadi.SX, cs.casadi.DM]) -> None:
+        """Add linear constraint.
+
+        Adds a constraint that is assumed to be linear in x (the
+        decision variables). When the constraint is not linear, an
+        AssertionError is thrown. Note, the constraint is defined as
+
+            lbc <= c <= ubc.
+
+        This is included in the linear constraint variable container
+        as two constraints, i.e.
+
+            c - lbc >= 0, and
+            ubc - c >= 0.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the linear constraint.
+
+        lbc : Union[cs.casadi.SX, cs.casadi.DM]
+            Lower bound for constraint.
+
+        c : Union[cs.casadi.SX, cs.casadi.DM]
+            The constraint array.
+
+        ubc : Union[cs.casadi.SX, cs.casadi.DM]
+            Upper bound for the constraint.
+
+        """
         x = self.decision_variables.vec()
         lb = c - lbc
         ub = ubc - c
@@ -70,7 +252,45 @@ class OptimizationBuilder:
         self.lin_constraints[name+'_lb'] = lb
         self.lin_constraints[name+'_ub'] = ub
 
-    def add_ineq_constraint(self, name, lbc, c, ubc):
+    def add_ineq_constraint(
+            self,
+            name: str,
+            lbc: Union[cs.casadi.SX, cs.casadi.DM],
+            c: cs.casadi.SX,
+            ubc: Union[cs.casadi.SX, cs.casadi.DM]) -> None:
+        """Adds an inequality constraint to the optimization problem.
+
+        Note, the constraint is defined as
+
+            lbc <= c <= ubc.
+
+        This is included in the linear constraint variable container
+        as two constraints, i.e.
+
+            c - lbc >= 0, and
+            ubc - c >= 0.
+
+        If the constraint is linear then it is added as a linear
+        constraint (i.e. it will be added to the lin_constraints
+        attribute, otherwise it will be logged in the ineq_constraints
+        attribute.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the constraint.
+
+        lbc : Union[cs.casadi.SX, cs.casadi.DM]
+            Lower bound for constraint.
+
+        c : Union[cs.casadi.SX, cs.casadi.DM]
+            The constraint array.
+
+        ubc : Union[cs.casadi.SX, cs.casadi.DM]
+            Upper bound for the constraint.
+
+        """
         x = self.decision_variables.vec()
         lb = c - lbc
         ub = ubc - c
@@ -81,16 +301,159 @@ class OptimizationBuilder:
         self.ineq_constraints[name+'_lb'] = lb
         self.ineq_constraints[name+'_ub'] = ub
 
-    def add_eq_constraint(self, name, c, eqc):
+    def add_eq_constraint(
+            self,
+            name: str,
+            lhsc: Union[cs.casadi.SX, cs.casadi.DM],
+            rhsc: Optional[Union[cs.casadi.SX, cs.casadi.DM]]=None) -> None:
+        """Adds an equality constraint to the optimization problem.
+
+        Note, the constraint is defined as lhsc == rhsc.
+
+        If the constraint is linear then it is added as a linear
+        constraint (i.e. it will be added to the lin_constraints
+        attribute, otherwise it will be logged in the eq_constraints
+        attribute.
+
+        Parameters
+        ----------
+
+        name : str
+            Name for the constraint.
+
+        lhsc : Union[cs.casadi.SX, cs.casadi.DM]
+            Left hand side of the equality constraint.
+
+        rhsc : Union[cs.casadi.SX, cs.casadi.DM] (default is None)
+            Right hand side of the equality constraint. This is
+            optional, if it is None then it is assumed to be the zero
+            array with the same shape as lhsc.
+
+        """
+        if rhsc is None:
+            rhsc = cs.DM.zeros(*lhsc.shape)
         x = self.decision_variables.vec()
-        eq = c - eqc
+        eq = lhsc - rhsc
         if cs.is_linear(eq, x):
             print(f"[WARN] given constraint '{name}' is linear in x, adding as linear contraint")
-            self.add_lin_constraint(name, eqc, c, eqc)
+            self.add_lin_constraint(name, lhsc, rhsc, lhsc) # (lhs=rhs) is equiv to (lhs <= rhs and rhs <= lhs)
             return
         self.eq_constraints[name] = eq
 
-    def build(self):
+    def build(self) -> Union[UnconstrainedQP,\
+                             LinearConstrainedQP, \
+                             NonlinearConstrainedQP, \
+                             UnconstrainedOptimization, \
+                             LinearConstrainedOptimization, \
+                             NonlinearConstrainedOptimization]:
+        """Build the optimization problem.
+
+        For the given 
+
+        - decision variables (x),
+        - parameters (p),
+        - cost function (cost), and
+        - constraints (k/g/h)
+
+        the approrpriate optimization problem is built. The type of
+        the optimization problem is chosen depending on the cost
+        function and constraints. The available problem types are as
+        follows. Note, 
+
+        - dash "'" means transpose and full-stop "." means the dot
+          product, or matrix-matrix/matrix-vector multiplication,
+        - 0 is used to denote the zero array with an appropriate
+          dimension,
+        - equality constraints can be represented by inequality
+          constraints, i.e. lhs == rhs is equivalent to lhs <= rhs and
+          lhs >= rhs, and
+        - the problem type determines the solvers that are available
+          to solve the problem.
+
+        UnconstrainedQP: 
+        
+                min cost(x, p) where cost(x, p) = x'.P(p).x + x'.q(p)
+                 x
+        
+            The problem is unconstrained, and has quadratic cost
+            function - note, P and q are derived from the given cost
+            function (you don't have to explicitly state P/q).
+
+        LinearConstrainedQP:
+
+                min cost(x, p) where cost(x, p) = x'.P(p).x + x'.q(p)
+                 x
+
+                subject to k(x, p) = M(p).x + c(p) >= 0
+
+            The problem is constrained by only linear constraints and
+            has a quadratic cost function - note, P/M and q/c are
+            derived from the given cost function and constraints (you
+            don't have to explicitly state P/q/M/c).
+
+        NonlinearConstrainedQP:
+
+                min cost(x, p) where cost(x) = x'.P(p).x + x'.q
+                 x
+
+                subject to 
+
+                    k(x, p) = M(p).x + c(p) >= 0,
+                    g(x) == 0, and 
+                    h(x) >= 0
+
+            The problem is constrained by nonlinear constraints and
+            has a quadratic cost function - note, P/M and q/c are
+            derived from the given cost function and constraints (you
+            don't have to explicitly state P/q/M/c).
+
+        UnconstrainedOptimization:
+
+                min cost(x, p)
+                 x        
+
+            The problem is unconstrained and the cost function is
+            nonlinear in x.
+
+        LinearConstrainedOptimization:
+
+                min cost(x, p)
+                 x
+
+                subject to k(x, p) = M(p).x + c(p) >= 0
+
+            The problem is constrained with linear constraints and has
+            a nonlinear cost function in x.
+
+        NonlinearConstrainedOptimization:
+
+                min cost(x, p)
+                 x
+
+                subject to 
+
+                    k(x, p) = M(p).x + c(p) >= 0,
+                    g(x) == 0, and 
+                    h(x) >= 0
+
+            The problem is constrained by nonlinear constraints and
+            has a nonlinear cost function.
+
+        Returns
+        -------
+
+        opt_problem : Union[UnconstrainedQP, 
+                            LinearConstrainedQP,
+                            NonlinearConstrainedQP,
+                            UnconstrainedOptimization,
+                            LinearConstrainedOptimization,
+                            NonlinearConstrainedOptimization]
+            The optimization problem of either one of the above
+            types. The problem type determines what costraints (and
+            their type) are available and also the structure of the
+            cost function.
+
+        """
 
         # Get decision variables and parameters as SX column vectors
         x = self.decision_variables.vec()
--- pyinvk/optimization.py
+++ pyinvk/optimization.py
@@ -21,6 +21,7 @@ class _QuadraticCost:
     """
 
     def P(self, p):
+        """The P(p) array in a quadratic cost term where p are parameters."""
 
         # Create x SX variables
         x = self.decision_variables.vec()
@@ -30,6 +31,7 @@ class _QuadraticCost:
         return 0.5*cs.DM(ddf)
 
     def q(self, p):
+        """The q(p) array in a quadratic cost term where p are parameters."""
 
         # Create x SX variables
         x = self.decision_variables.vec()
@@ -53,6 +55,7 @@ class _LinearConstraints:
     """
 
     def __init__(self):
+        """Constructor for the _LinearConstraints class."""
         self.k = None
         self.dk = None
         self.ddk = None
@@ -60,17 +63,21 @@ class _LinearConstraints:
 
     @property
     def nk(self):
+        """The number of linear constraints."""
         return self.k.numel_out()
 
     @property
     def lbk(self):
+        """Lower bound for the linear constraints (i.e. the zero array)."""
         return cs.DM.zeros(self.nk)
 
     @property
     def ubk(self):
+        """Upper bound for the linear constraints (i.e. an array with large numbers used instead of infinity)."""
         return big_number*cs.DM.ones(self.nk)
 
     def M(self, p):
+        """The M(p) array in the linear constraints where p are parameters."""
 
         # Create x SX variables
         x = self.decision_variables.vec()
@@ -80,6 +87,7 @@ class _LinearConstraints:
         return cs.DM(dk)
 
     def c(self, p):
+        """The c(p) array in the linear constraints where p are parameters."""
 
         # Create zeros x
         x_zero = cs.DM.zeros(self.nx)
@@ -90,18 +98,23 @@ class _LinearConstraints:
 
     @property
     def nr(self):
+        """The number of linear constraints for r(x, p)."""
         return self.nk
 
     def r(self, x, p):
+        """The constraint vector r(x, p)."""
         return self.M(p) @ cs.vec(x)
 
     def dr(self, x, p):
+        """The first deriviative for the constraint vector r(x, p) with respect to x."""
         return self.M(p)
 
     def lbr(self, p):
+        """Lower bound for the constraint vector r(x, p), i.e. it is defined as -c(p)."""
         return -self.c(p)
 
     def ubr(self, p):
+        """Upper bound for the constraint vector r(x, p), i.e. it is an array with large numbers used instead of infinity."""
         return self.ubk
 
 class _NonlinearEqualityConstraints:
@@ -113,6 +126,7 @@ class _NonlinearEqualityConstraints:
     """
 
     def __init__(self):
+        """Constructor for the _NonlinearEqualityConstraint class."""
         self.g = None
         self.dg = None
         self.ddg = None
@@ -120,14 +134,17 @@ class _NonlinearEqualityConstraints:
 
     @property
     def ng(self):
+        """Number of constraints."""
         return self.g.numel_out()
 
     @property
     def lbg(self):
+        """Lower bound for the constraints, i.e. an array of zeros."""
         return cs.DM.zeros(self.ng)
 
     @property
     def ubg(self):
+        """Upper bound for the constraints, i.e. an array of zeros."""
         return self.lbg
 
 class _NonlinearInequalityConstraints:
@@ -146,14 +163,17 @@ class _NonlinearInequalityConstraints:
 
     @property
     def nh(self):
+        """Number of constraints."""
         return self.h.numel_out()
 
     @property
     def lbh(self):
+        """Lower bound for the constraints, i.e. an array of zeros."""
         return cs.DM.zeros(self.nh)
 
     @property
     def ubh(self):
+        """Upper bound for the constraints, i.e. an array of large numbers instead of infinity."""
         return big_number*cs.DM.ones(self.nh)
 
 class _NonlinearConstraints:
@@ -161,28 +181,35 @@ class _NonlinearConstraints:
     """Additional methods u, v for (in)equality nonlinear constraints."""
 
     def u(self, x, p):
+        """Constraint array where u=[k', g', h']'."""
         return cs.vertcat(self.k(x, p), self.g(x, p), self.h(x, p))
 
     def du(self, x, p):
+        """The first derivative of u with respect to x."""
         return cs.vertcat(self.dk(x, p), self.dg(x, p), self.dh(x, p))
 
     def ddu(self, x, p):
+        """The second derivative of u with respect to x."""
         return cs.vertcat(self.ddk(x, p), self.ddg(x, p), self.ddh(x, p))
 
     @property
     def nu(self):
+        """The number of constraints in the u array."""
         return self.nk + self.ng + self.nh
 
     @property
     def lbu(self):
+        """The lower bound for the u array."""
         return cs.vertcat(self.lbk, self.lbg, self.lbh)
 
     @property
     def ubu(self):
+        """The upper bound for the u array."""
         return cs.vertcat(self.ubk, self.ubg, self.ubh)
 
 
     def v(self, x, p):
+        """Constraint array where v=[k', g', -g', h']' >= 0."""
         return cs.vertcat(
             self.k(x, p),
             self.g(x, p),
@@ -191,14 +218,16 @@ class _NonlinearConstraints:
         )
 
     def dv(self, x, p):
+        """The first derivative of the constraints v with respect to x."""
         return cs.vertcat(
             self.dk(x, p),
             self.dg(x, p),
-            -self.dg(x, p),            
+            -self.dg(x, p),
             self.dh(x, p),
         )
 
     def ddv(self, x, p):
+        """The second derivative of the constraints v with respect to x."""
         return cs.vertcat(
             self.ddk(x, p),
             self.ddg(x, p),
@@ -208,14 +237,17 @@ class _NonlinearConstraints:
 
     @property
     def nv(self):
+        """The number of constraints in the vector v."""
         return self.nk + 2*self.ng + self.nh
 
     @property
     def lbv(self):
+        """Lower bound for the constraints v, i.e. the zero array."""
         return cs.DM.zeros(self.nv)
 
     @property
     def ubv(self):
+        """Upper bound for the constraints v, i.e. an array of large numbers instead of infinity."""
         return cs.vertcat(self.ubk, cs.DM.zeros(2*self.ng), self.ubh)
 
 ############################################################################
@@ -223,7 +255,7 @@ class _NonlinearConstraints:
 
 class _Optimization:
 
-    """Base optimization class, assumes decision variables, parameters, and cost function"""
+    """Base optimization class, assumes decision variables (x), parameters (p), and cost function (f)."""
 
     def __init__(self):
         self.f = None
@@ -235,17 +267,31 @@ class _Optimization:
 
     @property
     def nx(self):
+        """Number of decision variables."""
         return self.decision_variables.numel()
 
     @property
     def np(self):
+        """Number of parameters."""
         return self.parameters.numel()
 
 class UnconstrainedQP(
         _Optimization,
         _QuadraticCost):
 
+    """Unconstrained Quadratic Program.
+
+            min cost(x, p) where cost(x, p) = x'.P(p).x + x'.q(p)
+             x
+
+    The problem is unconstrained, and has quadratic cost function -
+    note, P and q are derived from the given cost function (you don't
+    have to explicitly state P/q).
+
+    """
+
     def __init__(self):
+        """Constructor for the UnconstrainedQP problem."""
         _Optimization.__init__(self)
         _QuadraticCost.__init__(self)
 
@@ -253,7 +299,23 @@ class LinearConstrainedQP(
         UnconstrainedQP,
         _LinearConstraints):
 
+    """Linear constrained Quadratic Program.
+
+            min cost(x, p) where cost(x, p) = x'.P(p).x + x'.q(p)
+                 x
+
+                subject to k(x, p) = M(p).x + c(p) >= 0
+
+    The problem is constrained by only linear constraints and has a
+    quadratic cost function - note, P/M and q/c are derived from the
+    given cost function and constraints (you don't have to explicitly
+    state P/q/M/c).
+    
+    """
+
     def __init__(self):
+
+    """Constructor for the LinearConstrainedQP problem."""
         UnconstrainedQP.__init__(self)
         _LinearConstraints.__init__(self)
 
@@ -264,42 +326,89 @@ class NonlinearConstrainedQP(
         _NonlinearInequalityConstraints,
         _NonlinearConstraints):
 
+    """Nonlinear constrained Quadratic Program. 
+
+            min cost(x, p) where cost(x) = x'.P(p).x + x'.q
+             x
+
+                subject to 
+
+                    k(x, p) = M(p).x + c(p) >= 0,
+                    g(x) == 0, and 
+                    h(x) >= 0
+
+    The problem is constrained by nonlinear constraints and has a
+    quadratic cost function - note, P/M and q/c are derived from the
+    given cost function and constraints (you don't have to explicitly
+    state P/q/M/c).
+
+    """
+
     def __init__(self):
+        """Constructor for the NonlinearConstrainedQP problem."""
         UnconstrainedQP.__init__(self)
         _LinearConstraints.__init__(self)
         _NonlinearEqualityConstraints.__init__(self)
         _NonlinearInequalityConstraints.__init__(self)
 
 class UnconstrainedOptimization(_Optimization):
+    """Unconstrained optimization problem.
+
+            min cost(x, p)
+             x        
+
+    The problem is unconstrained and the cost function is nonlinear in
+    x.
+
+    """
     pass
 
 class LinearConstrainedOptimization(
         UnconstrainedOptimization,
         _LinearConstraints):
+    """Linear constrained optimization problem.                
+
+
+        min cost(x, p)
+         x
+
+            subject to k(x, p) = M(p).x + c(p) >= 0
+
+    The problem is constrained with linear constraints and has a
+    nonlinear cost function in x.
+
+    """
 
     def __init__(self):
+        """Constructor for the LinearConstrainedOptimization problem."""        
         UnconstrainedOptimization.__init__(self)
         _LinearConstraints.__init__(self)
 
     def u(self, x, p):
+        """Constraint array equivalent to k."""
         return self.k(x, p)
 
     def du(self, x, p):
+        """The first derivative of the constraint array u with respect to x."""
         return self.dk(x, p)
 
     def ddu(self, x, p):
+        """The second derivative of the constraint array u with respect to x."""        
         return self.ddk(x, p)
 
     @property
     def nu(self):
+        """The number of constraints in the array u."""
         return self.k.numel_out()
 
     @property
     def lbu(self):
+        """Lower bound for the constraints u, i.e. the zero array."""
         return cs.DM.zeros(self.nu)
 
     @property
     def ubu(self):
+        """Upper bound for the constraints u, i.e. an array of large numbers instead of infinity."""
         return big_number*cs.DM(self.nu)
 
 class NonlinearConstrainedOptimization(
@@ -308,8 +417,24 @@ class NonlinearConstrainedOptimization(
         _NonlinearEqualityConstraints,
         _NonlinearInequalityConstraints,
         _NonlinearConstraints):
+    """Nonlinear constrained optimization problem.
+
+        min cost(x, p)
+         x
+
+            subject to 
+
+                k(x, p) = M(p).x + c(p) >= 0,
+                g(x) == 0, and 
+                h(x) >= 0
+
+    The problem is constrained by nonlinear constraints and has a
+    nonlinear cost function.
+
+    """
 
     def __init__(self):
+        """Constructor for the NonlinearConstrainedOptimization problem."""                
         UnconstrainedOptimization.__init__(self)
         _LinearConstraints.__init__(self)
         _NonlinearEqualityConstraints.__init__(self)
