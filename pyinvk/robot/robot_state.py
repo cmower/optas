e13e5c7ff723968540828c42fbc0edd330426d3c
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-08-11 18:06:08 +0100

    Remove robot/ (maybe another time)

--- pyinvk/robot/robot_state.py
+++
@@ -1,9 +0,0 @@
-
-class RobotState:
-
-    def __init__(self):
-        pass
-
-    def getJacobian(self):
-        root_joint_model = self._group.getJointModels()[0]
-        root_link_model = root_joint_model.getParentLinkModel()
