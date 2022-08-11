b4569307365f2ec0b38650cb566c7b4916d04545
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-08-11 18:00:21 +0100

    Remove license

--- pyinvk/spatialmath.py
+++ pyinvk/spatialmath.py
@@ -1,37 +1,6 @@
 import casadi as cs
 from casadi import sin, cos, vec
 
-""" 
-
-The code in this script is a ported version of Peter Corke's
-spatialmath library using Python/CasADi. The license is provided
-below.
-
-MIT License
-
-Copyright (c) 2019 Peter Corke
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-
-"""
-
 eps = cs.np.finfo(float).eps
 
 _arraylike_types = (cs.DM, cs.SX, list, tuple, cs.np.ndarray, float, int)
