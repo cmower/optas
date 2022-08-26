37a259919a409863f462d9f43a5bc649a043773e
Chris E. Mower <christopher.mower@kcl.ac.uk>
2022-08-26 18:24:38 +0100

    Bring all casadi functionality to optas

--- optas/__init__.py
+++ optas/__init__.py
@@ -1,4 +1,5 @@
 import casadi as cs
+from casadi import *
 from .models import RobotModel, TaskModel
 from .builder import OptimizationBuilder
 from .solver import CasADiSolver, OSQPSolver, CVXOPTSolver, ScipyMinimizeSolver
