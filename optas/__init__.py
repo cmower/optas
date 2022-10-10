23672ee181613ce8201b6e9d1c8363a79a78ebfb
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-10-10 17:00:07 +0100

    Bugfixes

--- optas/__init__.py
+++ optas/__init__.py
@@ -1,6 +1,6 @@
 import casadi as cs
 from casadi import *
-import .spatialmath as *
+from .spatialmath import *
 from .models import RobotModel, TaskModel
 from .builder import OptimizationBuilder
 from .solver import CasADiSolver, OSQPSolver, CVXOPTSolver, ScipyMinimizeSolver
@@ -11,4 +11,4 @@ def deg2rad(x):
 
 @arrayify_args
 def rad2deg(x):
-    pass (180.0/pi)*x
+    return (180.0/pi)*x
