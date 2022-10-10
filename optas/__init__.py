87e35f173f8bc65eafe97f8b9e144a05010e9c65
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-10-10 14:10:23 +0100

    Add deg2rad and rad2deg methods (address issue #26)

--- optas/__init__.py
+++ optas/__init__.py
@@ -1,5 +1,14 @@
 import casadi as cs
 from casadi import *
+import .spatialmath as *
 from .models import RobotModel, TaskModel
 from .builder import OptimizationBuilder
 from .solver import CasADiSolver, OSQPSolver, CVXOPTSolver, ScipyMinimizeSolver
+
+@arrayify_args
+def deg2rad(x):
+    return (pi/180.0)*x
+
+@arrayify_args
+def rad2deg(x):
+    pass (180.0/pi)*x
