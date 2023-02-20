509659a4848f1944c7e095a542dba2642a3a1cfe
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-02-20 00:50:37 +0000

    Implement tests (WIP)

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,4 +1,5 @@
 import optas
+from optas.sx_container import SXContainer
 import numpy as np
 
 NUM_RANDOM = 100
@@ -46,3 +47,71 @@ def test_vertcon():
     x = optas.SX.sym("x", 2)
     p = optas.SX.sym("p", 2)
 
+    ineq = [
+        optas.Function("i1", [x, p], [p[0] * x[0]]),
+        optas.Function("i2", [x, p], [-p[1] * x[1] ** 2]),
+    ]
+
+    eq = [
+        optas.Function("e", [x, p], [2 * x[0] - x[1]]),
+    ]
+
+    fun = optas.optimization._vertcon(x, p, ineq=ineq, eq=eq)
+
+    f_known = optas.vertcat(
+        p[0] * x[0],
+        -p[1] * x[1] ** 2,
+        2 * x[0] - x[1],
+        -2 * x[0] + x[1],
+    )
+    fun_known = optas.Function("v_known", [x, p], [f_known])
+
+    for _ in range(NUM_RANDOM):
+        x = np.random.uniform(-10, 10, size=(2,))
+        p = np.random.uniform(-10, 10, size=(2,))
+        assert isclose(fun_known(x, p).toarray(), fun(x, p).toarray())
+
+
+def test_optimization():
+    dv = SXContainer()
+    dv["x"] = optas.SX.sym("x", 2, 3)
+    x = dv.vec()
+
+    pr = SXContainer()
+    pr["p"] = optas.SX.sym("p")
+    p = pr.vec()
+
+    ct = SXContainer()
+    ct["c"] = p * optas.sumsqr(x)
+
+    models = [
+        optas.TaskModel("test_model_1", 2),
+        optas.TaskModel("test_model_2", 3),
+    ]
+
+    opt = optas.optimization.Optimization(dv, pr, ct)
+    opt.set_models(models)
+
+    assert opt.models == models
+    assert opt.nx == 6
+    assert opt.np == 1
+
+    assert opt.f([1, 2, 3, 4, 5, 6], [3]) == 273
+
+def test_QuadraticCostUnconstrained():
+    pass
+
+def test_QuadraticCostLinearConstraints():
+    pass
+
+def test_QuadraticCostNonlinearConstraints():
+    pass
+
+def test_NonlinearCostUnconstrained():
+    pass
+
+def test_NonlinearCostLinearConstraints():
+    pass
+
+def test_NonlinearCostNonlinearConstraints():
+    pass
---
+++ tests/test_sx_container.py
@@ -0,0 +1,31 @@
+import optas
+
+def test_add():
+    pass
+
+def test_setitem():
+    pass
+
+def test_variable_is_discrete():
+    pass
+
+def test_has_discrete_variables():
+    pass
+
+def test_discrete():
+    pass
+
+def test_vec():
+    pass
+
+def test_numel():
+    pass
+
+def test_vec2dict():
+    pass
+
+def test_dict2vec():
+    pass
+
+def test_zero():
+    pass
