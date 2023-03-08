3c9155d5af1effea0b5f4606381f1d8dc99db471
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-03-08 00:47:17 +0000

    Bugfix

--- optas/sx_container.py
+++ optas/sx_container.py
@@ -17,7 +17,7 @@ class SXContainer(collections.OrderedDict):
     ## Dict[str, bool]: labels for each item in dict, true means variables are discrete
     is_discrete = {}
 
-    def __add__(self, other: SXContainer) -> SXContainer:
+    def __add__(self, other):
         """! Add two SXContainer's.
 
         @param other Another SXContainer instance.
