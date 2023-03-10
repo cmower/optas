6b2f4651c9047b09243cb9e5e7d17ad627871382
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-03-10 15:16:38 +0000

    Update workflow (and remove other tests in test_examples.py to speed up result)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -31,59 +31,59 @@ def load_main_function(filename):
     return main
 
 
-def test_dual_arm():
-    main = load_main_function("dual_arm.py")
-    assert main(gui=False) == 0
+# def test_dual_arm():
+#     main = load_main_function("dual_arm.py")
+#     assert main(gui=False) == 0
 
 
-def test_planar_idk():
-    main = load_main_function("planar_idk.py")
-    assert main() == 0
+# def test_planar_idk():
+#     main = load_main_function("planar_idk.py")
+#     assert main() == 0
 
 
-def test_planar_ik():
-    main = load_main_function("planar_ik.py")
-    assert main() == 0
+# def test_planar_ik():
+#     main = load_main_function("planar_ik.py")
+#     assert main() == 0
 
 
-def test_figure_eight_plan():
-    main = load_main_function("figure_eight_plan.py")
-    assert main(gui=False) == 0
+# def test_figure_eight_plan():
+#     main = load_main_function("figure_eight_plan.py")
+#     assert main(gui=False) == 0
 
 
-def test_figure_eight_plan_6dof():
-    main = load_main_function("figure_eight_plan_6dof.py")
-    assert main(gui=False) == 0
+# def test_figure_eight_plan_6dof():
+#     main = load_main_function("figure_eight_plan_6dof.py")
+#     assert main(gui=False) == 0
 
 
-def test_point_mass_mpc():
-    main = load_main_function("point_mass_mpc.py")
-    assert main(show=False) == 0
+# def test_point_mass_mpc():
+#     main = load_main_function("point_mass_mpc.py")
+#     assert main(show=False) == 0
 
 
-def test_point_mass_planner():
-    main = load_main_function("point_mass_planner.py")
-    assert main(show=False) == 0
+# def test_point_mass_planner():
+#     main = load_main_function("point_mass_planner.py")
+#     assert main(show=False) == 0
 
 
-def test_pushing():
-    main = load_main_function("pushing.py")
-    assert main(gui=False) == 0
+# def test_pushing():
+#     main = load_main_function("pushing.py")
+#     assert main(gui=False) == 0
 
 
-def test_pushing():
-    main = load_main_function("pushing.py")
-    assert main(gui=False) == 0
+# def test_pushing():
+#     main = load_main_function("pushing.py")
+#     assert main(gui=False) == 0
 
 
-def test_pybullet_api():
-    main = load_main_function("pybullet_api.py")
-    assert main(gui=False) == 0
+# def test_pybullet_api():
+#     main = load_main_function("pybullet_api.py")
+#     assert main(gui=False) == 0
 
 
-def test_simple_joint_space_planner():
-    main = load_main_function("simple_joint_space_planner.py")
-    assert main(gui=False) == 0
+# def test_simple_joint_space_planner():
+#     main = load_main_function("simple_joint_space_planner.py")
+#     assert main(gui=False) == 0
 
 
 def test_example():
