46c6250465662493e6c34c2c8033ead19f9c17e7
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-03-10 16:36:52 +0000

    Minor

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -89,7 +89,7 @@ def load_main_function(filename):
 def test_example():
 
     os.system(
-        "Xvfb :1 -screen 0 1600x1200x16  &"
+        "Xvfb :1 -screen 0 1600x1200x16  "
     )  # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
     os.environ["DISPLAY"] = ":1"  # tell X clients to use our virtual DISPLAY :1
 
