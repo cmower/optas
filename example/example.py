4cc5ab25a777a963774fa87e6ae1f68be7f50f9e
TianHuanyu <fsfsthy@gmail.com>
2023-03-10 12:50:30 +0000

    Follow-up delete

--- example/example.py
+++ example/example.py
@@ -60,7 +60,6 @@ solution = solver.solve()
 q_solution = solution[f"{name}/q"]
 
 # Visualize the robot
-# params = {"link_axis_scale": 0.5}
 vis = optas.Visualizer()  # solution
 
 # Draw goal position and start visualizer
