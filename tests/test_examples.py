1baf49d4fb089d865d88b2f4dc15c25beee73272
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-03-10 16:37:58 +0000

    Revert changes and comment out test_example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import time
 import pathlib
-import threading
 import importlib
 
 optas_path = pathlib.Path(
@@ -31,67 +29,61 @@ def load_main_function(filename):
     return main
 
 
-# def test_dual_arm():
-#     main = load_main_function("dual_arm.py")
-#     assert main(gui=False) == 0
+def test_dual_arm():
+    main = load_main_function("dual_arm.py")
+    assert main(gui=False) == 0
 
 
-# def test_planar_idk():
-#     main = load_main_function("planar_idk.py")
-#     assert main() == 0
+def test_planar_idk():
+    main = load_main_function("planar_idk.py")
+    assert main() == 0
 
 
-# def test_planar_ik():
-#     main = load_main_function("planar_ik.py")
-#     assert main() == 0
+def test_planar_ik():
+    main = load_main_function("planar_ik.py")
+    assert main() == 0
 
 
-# def test_figure_eight_plan():
-#     main = load_main_function("figure_eight_plan.py")
-#     assert main(gui=False) == 0
+def test_figure_eight_plan():
+    main = load_main_function("figure_eight_plan.py")
+    assert main(gui=False) == 0
 
 
-# def test_figure_eight_plan_6dof():
-#     main = load_main_function("figure_eight_plan_6dof.py")
-#     assert main(gui=False) == 0
+def test_figure_eight_plan_6dof():
+    main = load_main_function("figure_eight_plan_6dof.py")
+    assert main(gui=False) == 0
 
 
-# def test_point_mass_mpc():
-#     main = load_main_function("point_mass_mpc.py")
-#     assert main(show=False) == 0
+def test_point_mass_mpc():
+    main = load_main_function("point_mass_mpc.py")
+    assert main(show=False) == 0
 
 
-# def test_point_mass_planner():
-#     main = load_main_function("point_mass_planner.py")
-#     assert main(show=False) == 0
+def test_point_mass_planner():
+    main = load_main_function("point_mass_planner.py")
+    assert main(show=False) == 0
 
 
-# def test_pushing():
-#     main = load_main_function("pushing.py")
-#     assert main(gui=False) == 0
+def test_pushing():
+    main = load_main_function("pushing.py")
+    assert main(gui=False) == 0
 
 
-# def test_pushing():
-#     main = load_main_function("pushing.py")
-#     assert main(gui=False) == 0
+def test_pushing():
+    main = load_main_function("pushing.py")
+    assert main(gui=False) == 0
 
 
-# def test_pybullet_api():
-#     main = load_main_function("pybullet_api.py")
-#     assert main(gui=False) == 0
+def test_pybullet_api():
+    main = load_main_function("pybullet_api.py")
+    assert main(gui=False) == 0
 
 
-# def test_simple_joint_space_planner():
-#     main = load_main_function("simple_joint_space_planner.py")
-#     assert main(gui=False) == 0
+def test_simple_joint_space_planner():
+    main = load_main_function("simple_joint_space_planner.py")
+    assert main(gui=False) == 0
 
 
-def test_example():
-
-    os.system(
-        "Xvfb :1 -screen 0 1600x1200x16  "
-    )  # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
-    os.environ["DISPLAY"] = ":1"  # tell X clients to use our virtual DISPLAY :1
-
-    path = examples_path / "example.py"
-    module = import_module(path)
+# def test_example():
+#     path = examples_path / "example.py"
+#     module = import_module(path)
